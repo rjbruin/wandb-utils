c9e45f180af1296d651c5fa554e0880297f3ec0a
Robert-Jan Bruintjes <r.j.bruintjes@gmail.com>
2021-11-02 14:10:03 +0100

    Add cleanup_artifacts.py.

---
+++ cleanup_artifacts.py
@@ -0,0 +1,46 @@
+"""Clean up artifacts from specified W&B project.
+
+Usage:
+    cleanup_artifacts.py <entity> <project> [--dry-run]
+
+Options:
+    --dry-run   Don't apply changes.
+"""
+import wandb
+import tqdm
+from docopt import docopt
+
+
+def main(args):
+    api = wandb.Api(overrides={
+        'entity': args['<entity>'],
+        'project': args['<project>'],
+    })
+    models = list(api.artifact_type('model').collections())
+
+    removed = {}
+    desc = 'Removing models' if not args['--dry-run'] else 'Discovering models'
+    for i, model in tqdm.tqdm(enumerate(models), desc=desc, total=len(models)):
+        for j, version in enumerate(model.versions()):
+            if version.state == 'DELETED':
+                continue
+            if "latest" not in version.aliases and \
+            "best" not in version.aliases:
+                if not args['--dry-run']:
+                    version.delete()
+
+                if model.name not in removed:
+                    removed[model.name] = 0
+                removed[model.name] += 1
+
+    if not args['--dry-run']:
+        print(f"Removed model versions that were not \"latest\" or \"best\":")
+    else:
+        print(f"Found model versions that were not \"latest\" or \"best\":")
+    for key in removed:
+        print(f"  {key}: {removed[key]}")
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='Clean up artifacts 1.0')
+    main(arguments)
